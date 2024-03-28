a2acfbf657709194ad759b39d55e221edf54b7e3
moaz <moaz469654@gmail.com>
2024-03-28 14:25:07 -0700

    i am tired

--- main.c
+++ main.c
@@ -1,40 +1,19 @@
-#include "main.h"
-/***/
-int main(int argc, char *argv)
+#include "monty.h"
+
+execution_env_t execution_env = EXECUTION_ENV_INITIALIZER;
+
+/**
+ * main - Entry point for Monty Interpreter
+ * @argc: number of arguments
+ * @argv: array of character pointers to arguments
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on error
+*/
+int main(int argc, char *argv[])
 {
-    unsigned int line_number = 0;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    
-    stack_t *stack = NULL;
+	if (argc != 2)
+	{
+		usage_error();
+	}
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    
-    FILE * filename = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-
-    while(read = getline(&line, &len, filename))
-    {
-        line_number++;
-        char *opcode = strtok(line, " \n");
-        char *arg = strtok(NULL, " \n");
-        if (opcode)
-        {
-            execute_instruction(opcode, arg, line_number);
-        }
-    }
-    free(line);
-    fclose(filename);
-    free_stack(stack);
-    return 0;
+	return (execute_operations(argv[1]));
 }
